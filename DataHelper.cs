0a585c834cc895c28f9ee7b1c4924639d77fccb4
wesleyngweli <wesleyngweli@gmail.com>
2017-12-06 15:27:35 -0600

    first commit

---
+++ DataHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TddPractice
+{
+    public class DataHelper
+    {
+        DataModel DM = new DataModel();
+        public bool ValidateIsEven(int n)
+        {
+
+            return (n % 2 == 0);
+        }
+
+        public bool ValidateIsPopulated(List<int> n)
+        {
+            return (n.Count > 0);
+        }
+
+
+    }
+}
---
+++ DataModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TddPractice
+{
+    public class DataModel
+    {
+        public List<int> listOfInt = Enumerable.Range(1,20).ToList();
+    }
+}
